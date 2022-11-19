f135d74d58d2d83d471ac548c4dcd00a764b695e
lucky <brunosm0902@gmail.com>
2022-11-19 21:23:16 +0200

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 var typed = new Typed('#typed', {
   strings: ["^2000 Seja bem vindo!", "O tenis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."],
-  typeSpeed: 40
+  typeSpeed: 50
 });
 
 const sr = ScrollReveal({
   origin: 'top',
   distance: '50px',
-  duration: 5500,
+  duration: 2000,
   reset: true
 });
 
-ScrollReveal().reveal('.header p', { delay: 200 });
-ScrollReveal().reveal('.header h3', { delay: 400 });
+ScrollReveal().reveal('.header p', { delay: 100 });
+ScrollReveal().reveal('.header h3', { delay: 250 });
 
-ScrollReveal().reveal('#home .image', { delay: 200 });
-ScrollReveal().reveal('#home .text', { delay: 600 });
+ScrollReveal().reveal('#home .image', { delay: 100 });
+ScrollReveal().reveal('#home .text', { delay: 350 });
 
-ScrollReveal().reveal('#spotlight .text', { delay: 600 });
+ScrollReveal().reveal('#spotlight .text', { delay: 350 });
 
-ScrollReveal().reveal('.shoe', { delay: 800 });
-ScrollReveal().reveal('.footer', { delay: 800 });
+ScrollReveal().reveal('.shoe', { delay: 300 });
+ScrollReveal().reveal('.footer', { delay: 400 });
